61e76323f59b049b7656d72bceb59a08f1f1307e
Raszbanana <75260673+Raszbanana@users.noreply.github.com>
2021-12-20 11:46:32 +0100

    First commit

---
+++ src/main/java/exam/backend/entities/Municipality.java
@@ -0,0 +1,4 @@
+package exam.backend.entities;
+
+public class Municipality {
+}
---
+++ src/test/java/exam/backend/MunicipalityApplicationTests.java
@@ -0,0 +1,13 @@
+package exam.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MunicipalityApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
